34794205482984ec91183b4b4562ff67a0e12b2d
Nripesh01 <magarnripesh@gmail.com>
2025-12-28 16:57:17 +0545

    views

--- exp_bud/models.py
+++ exp_bud/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.conf import settings
-
+# setting is used instead of User model so Django can work with either default User model or custom User model 
+#  without breaking your code.
+    #      - "auth.User" (default)
+    #      - "accounts.User" (custom)
 
 class Group(models.Model):
     name = models.CharField(max_length=100)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='groups_created')
     # AUTH_USER_MODEL tells Django which User model your project uses for authentication.
+    # This ensures:
+        # - Works today with default User
+        # - Works tomorrow with custom User
+        # - No database breaks
     # By default, Django uses:
          # -'auth.User' --> Django’s built-in User model
     created_at = models.DateTimeField(auto_now_add=True)
--- exp_bud/views.py
+++ exp_bud/views.py
@@ -12,4 +12,30 @@ class GroupListCreateView(generics.ListCreateAPIView):
     serializer_class = GroupSerializer
     
     def get_queryset(self):
-        return Group.objects.filter()
+        return Group.objects.filter(member__user=self.request.user).distinct()
+        # show only groups where current user is a member
+    
+    def perform_create(self, serializer):
+        group = serializer.save(created_by=self.request.user)
+         # creator automatically becomes a member
+        GroupMember.objects.create(group=group, user=self.request.user)
+        
+
+class AddMemberView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = AddMemberSerializer
+    
+    def post(self, request, group_id):
+        try:
+            group = Group.objects.get(id=group_id, member__user=request.user)
+        except Group.DoesNotExist:
+            return Response({'detail': 'Group not found'}, status=404)
+        
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        user_to_add = User.objects.get(username=serializer.validated_data['username'])
+        GroupMember.objects.get_or_create(group=group, user=user_to_add)
+        
+        return Response({'detail': 'Member is added'}, status=status.HTTP_201_CREATED)
+    
